5a4e420db350001a7a8a4f64368b5b595f3a4f07
OmniStream Dev <dev@omnistream.app>
2026-01-06 18:51:10 -0500

    fix: add apply false and flutter-plugin-loader to settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,15 +4,12 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    
-    plugins {
-        id("com.android.application") version "8.6.0"
-        id("org.jetbrains.kotlin.android") version "2.1.0"
-    }
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
