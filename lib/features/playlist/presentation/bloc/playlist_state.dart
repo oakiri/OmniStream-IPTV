98c67a23574627061e94096815ab1239e0c7c897
OmniStream Dev <dev@omnistream.app>
2026-01-07 08:39:23 -0500

    fix: Últimas correcciones críticas de la Fase 3 (minSdk 23, Category alias, imports verificados)

--- lib/features/playlist/presentation/bloc/playlist_state.dart
+++ lib/features/playlist/presentation/bloc/playlist_state.dart
@@ -1,4 +1,5 @@
 part of 'playlist_bloc.dart';
+import 'package:omnistream_iptv/features/playlist/domain/entities/category.dart' as entity;
 
 abstract class PlaylistState extends Equatable {
   const PlaylistState();
@@ -13,7 +14,7 @@ class PlaylistLoading extends PlaylistState {}
 
 class PlaylistLoaded extends PlaylistState {
   final List<Channel> channels;
-  final List<Category> categories;
+  final List<entity.Category> categories;
 
   const PlaylistLoaded({required this.channels, required this.categories});
 
